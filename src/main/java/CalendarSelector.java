7879a5551134c3eafd0b878a5954c883afb78cd9
serghiyk <serghiykataryna@gmail.com>
2018-10-19 16:41:20 +0300

    Added methods for Children/Infants drop-downs handling

--- src/main/java/CalendarSelector.java
+++ src/main/java/CalendarSelector.java
@@ -1,9 +1,12 @@
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import com.sun.istack.internal.NotNull;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import javax.swing.plaf.synth.SynthToolTipUI;
 import java.util.Calendar;
 import java.util.Random;
 
@@ -27,6 +30,30 @@ public class CalendarSelector {
     private ElementsCollection rightMonthBoxDays = $$(By.xpath("//div[@class='datepicker-dropdown']/div/div[3]//tbody//button"));
     private SelenideElement previousMonthButton = $(By.xpath("//div[@class='datepicker-dropdown']/div/button[1]"));
     private SelenideElement nextMonthButton = $(By.xpath("//div[@class='datepicker-dropdown']/div/button[2]"));
+
+    // Travelers
+    private SelenideElement travelersButton = $(By.xpath("//div[@id='traveler-selector-hp-package']//ul/li/button"));
+    private SelenideElement travelersPopUp = $(By.xpath("//li[@class='open']/div/div"));
+
+    private SelenideElement addNewRoom = $(By.xpath("//a[@class='secondary gcw-component gcw-component-initialized gcw-clone-field']"));
+
+    private ElementsCollection roomsForTravelers = $$(By.xpath("//li[@class='open']//div[contains(@class, 'traveler-selector-room-data target-clone-field')]"));
+    // *** For every room should be selected number of Adults, Children, Infants. Values can be increased and decreased by "+" and "-" buttons.
+    // *   Also, if there is an Infant, new section will be displayed with additional selectors
+
+    // Bellow need to ba applied to element from "roomsForTravelers" collection
+    private String decreaseValueForTravelersXPath = "..//button[@class='uitk-step-input-button uitk-step-input-minus']";
+    private String increaseValueForTravelersXPath = "..//button[@class='uitk-step-input-button uitk-step-input-plus']";
+    private String currentNumberOfPersonsXPath = "..//span[@class='uitk-step-input-value']";
+    private SelenideElement childrenSection = $(By.xpath("//div[@class='children-wrapper']"));
+    private SelenideElement infantsSection = $(By.xpath("//div[@class='infants-wrapper']"));
+    private String childrenAgeSelectorsXPath = "..//div[@class='children-wrapper']//select";
+    private String infantsAgeSelectorsXPath = "..//div[@class='infants-wrapper']//select";
+
+
+
+
+
 
     private SelenideElement searchButton = $("#search-button-hp-package");
 
@@ -217,6 +244,179 @@ public class CalendarSelector {
             return requiredElement.getAttribute("class").contains("end");
         else
             return false;
+    }
+
+    /****
+    Adjusting rooms quantity, visitors
+     ***/
+
+    public void openTravelersPopUp(){
+        if (!travelersPopUp.isDisplayed())
+            travelersButton.click();
+    }
+
+    private boolean addRoom(){
+        if (addNewRoom.isDisplayed()){
+            addNewRoom.click();
+            return true;
+        }
+        else {
+            System.out.println("Maximum rooms number reached");
+            return false;
+        }
+    }
+
+    private SelenideElement getElementForRoom(int numberOfTheRoom){
+        if (roomsForTravelers.size() < numberOfTheRoom){
+            System.out.println("Please add more rooms. Room #" + numberOfTheRoom + " is not available");
+            return null;
+        }
+        else
+            return roomsForTravelers.get(numberOfTheRoom - 1);
+    }
+
+    private int getNumberOfAdultsInRoom(int numberOfTheRoom){
+        SelenideElement room = getElementForRoom(numberOfTheRoom);
+        return Integer.parseInt(room.$$(By.xpath(currentNumberOfPersonsXPath)).get(0).getText());
+    }
+
+    private int getNumberOfChildrenInRoom(int numberOfTheRoom){
+        SelenideElement room = getElementForRoom(numberOfTheRoom);
+        return Integer.parseInt(room.$$(By.xpath(currentNumberOfPersonsXPath)).get(1).getText());
+    }
+
+    private int getNumberOfInfantsInRoom(int numberOfTheRoom){
+        SelenideElement room = getElementForRoom(numberOfTheRoom);
+        return Integer.parseInt(room.$$(By.xpath(currentNumberOfPersonsXPath)).get(2).getText());
+    }
+
+    public boolean setNumberOfAdultsForRoom(int overallAdults, int roomNumber){
+        SelenideElement room = getElementForRoom(roomNumber);
+        if (room != null){
+            int currentVisitors = getNumberOfAdultsInRoom(roomNumber);
+            if (currentVisitors < overallAdults) {
+                for (int i = currentVisitors; i < overallAdults; i++)
+                    room.$$(By.xpath(increaseValueForTravelersXPath)).get(0).click();
+            }
+            else if (currentVisitors > overallAdults){
+                for (int i = currentVisitors; i > overallAdults; i--)
+                    room.$$(By.xpath(decreaseValueForTravelersXPath)).get(0).click();
+            }
+        }
+        return overallAdults == getNumberOfAdultsInRoom(roomNumber);
+    }
+
+    public boolean setNumberOfChildrenForRoom(int overallChildren, int roomNumber){
+        SelenideElement room = getElementForRoom(roomNumber);
+        if (room != null){
+            int currentVisitors = getNumberOfChildrenInRoom(roomNumber);
+            if (currentVisitors < overallChildren) {
+                for (int i = currentVisitors; i < overallChildren; i++)
+                    room.$$(By.xpath(increaseValueForTravelersXPath)).get(1).click();
+            }
+            else if (currentVisitors > overallChildren){
+                for (int i = currentVisitors; i > overallChildren; i--)
+                    room.$$(By.xpath(decreaseValueForTravelersXPath)).get(1).click();
+            }
+        }
+        return overallChildren == getNumberOfChildrenInRoom(roomNumber);
+    }
+
+    public boolean setNumberOfInfantsForRoom(int overallInfants, int roomNumber){
+        SelenideElement room = getElementForRoom(roomNumber);
+        if (room != null){
+            int currentVisitors = getNumberOfInfantsInRoom(roomNumber);
+            if (currentVisitors < overallInfants) {
+                for (int i = currentVisitors; i < overallInfants; i++)
+                    room.$$(By.xpath(increaseValueForTravelersXPath)).get(2).click();
+            }
+            else if (currentVisitors > overallInfants){
+                for (int i = currentVisitors; i > overallInfants; i--)
+                    room.$$(By.xpath(decreaseValueForTravelersXPath)).get(2).click();
+            }
+        }
+        return overallInfants == getNumberOfInfantsInRoom(roomNumber);
+    }
+
+
+    // Dropdowns
+    private ElementsCollection childrenDropdownCollection(int roomNumber){
+        return getElementForRoom(roomNumber).$$(By.xpath(childrenAgeSelectorsXPath));
+    }
+
+    private ElementsCollection infantsDropdownCollection(int roomNumber){
+        return getElementForRoom(roomNumber).$$(By.xpath(infantsAgeSelectorsXPath));
+    }
+
+    public int getNumberOfDisplayedSelectorsForCollection(ElementsCollection collectionOfDropDowns){
+        int displayedSelectors = 0;
+        for (SelenideElement selector : collectionOfDropDowns){
+            if (selector.isDisplayed())
+                displayedSelectors++;
+        }
+        return displayedSelectors;
+    }
+
+    private SelenideElement getRequiredDropDown(ElementsCollection childrenOrInfantsCollection, int numberOfTheVisitor){
+        if (childrenOrInfantsCollection.size() < 1) {
+            System.out.println("No dropdown available");
+            return null;
+        }
+        else
+            return childrenOrInfantsCollection.get(numberOfTheVisitor - 1);
+    }
+
+    public boolean selectValueFromDropdown(SelenideElement dropDown, String value){
+        if (dropDown.isDisplayed()){
+//            dropDown.click();
+            try {
+                dropDown.selectOption(value);
+            }
+            catch (NoSuchElementException e){
+                System.out.println("Invalid value");
+            }
+            return dropDown.getText().equals(value);
+        }
+        else {
+            System.out.println("Dropdown is not available");
+            return false;
+        }
+    }
+
+    public boolean selectValueFromChildrenDropDown(int roomNumber, String value, int dropdownNumber){
+        if (dropdownNumber > getNumberOfDisplayedSelectorsForCollection(childrenDropdownCollection(roomNumber))){
+            System.out.println("Required dropdown is not displayed");
+            return false;
+        }
+        else {
+            SelenideElement dropdown = getRequiredDropDown(childrenDropdownCollection(roomNumber), dropdownNumber);
+            return selectValueFromDropdown(dropdown, value);
+        }
+    }
+
+    public boolean selectValueFromInfantsDropDown(int roomNumber, String value, int dropdownNumber){
+        if (dropdownNumber > getNumberOfDisplayedSelectorsForCollection(infantsDropdownCollection(roomNumber))){
+            System.out.println("Required dropdown is not displayed");
+            return false;
+        }
+        else {
+            SelenideElement dropdown = getRequiredDropDown(infantsDropdownCollection(roomNumber), dropdownNumber);
+            return selectValueFromDropdown(dropdown, value);
+        }
+    }
+
+
+    public boolean addRooms(int totalRequiredNumberOfRooms) {
+        openTravelersPopUp();
+        if (roomsForTravelers.size() < totalRequiredNumberOfRooms && roomsForTravelers.size() <= 3){
+            for (int i = roomsForTravelers.size(); i < totalRequiredNumberOfRooms; i ++)
+                addRoom();
+        }
+        else if (totalRequiredNumberOfRooms > 3){
+            System.out.println("3 rooms - maximum");
+            return false;
+        }
+        return roomsForTravelers.size() == totalRequiredNumberOfRooms;
     }
 
 
--- src/test/java/testClass.java
+++ src/test/java/testClass.java
@@ -41,13 +41,52 @@ public class testClass extends ConfigurationFile{
         Assert.assertTrue(calendarSelector.isReturnDateSelected(day, month, year));
     }
 
-    @Test(priority = 4)
-    public void findFlight(){
-        calendarSelector.fillAndSelectDepartureAddress("Lviv");
-        calendarSelector.fillAndSelectDestinationAddress("London");
-        calendarSelector.selectDepartureDate(23, 11, 2018);
-        calendarSelector.selectReturningDate(28, 11, 2018);
-        calendarSelector.clickAtSearchButton();
-        Assert.assertTrue(calendarSelector.getURL().contains("https://www.expedia.com/Hotel-Search"));
+//    @Test(priority = 4)
+//    public void findFlight(){
+//        calendarSelector.fillAndSelectDepartureAddress("Lviv");
+//        calendarSelector.fillAndSelectDestinationAddress("London");
+//        calendarSelector.selectDepartureDate(23, 11, 2018);
+//        calendarSelector.selectReturningDate(28, 11, 2018);
+//        calendarSelector.clickAtSearchButton();
+//        Assert.assertTrue(calendarSelector.getURL().contains("https://www.expedia.com/Hotel-Search"));
+//    }
+
+    @Test(priority = 5)
+    public void addMoreRooms(){
+        Assert.assertTrue(calendarSelector.addRooms(3));
     }
+
+    @Test(priority = 6)
+    public void setAdultsInTheRoom(){
+        calendarSelector.openTravelersPopUp();
+        Assert.assertTrue(calendarSelector.setNumberOfAdultsForRoom(4, 1));
+    }
+
+    @Test(priority = 6)
+    public void setChildrenInTheRoom(){
+        calendarSelector.openTravelersPopUp();
+        Assert.assertTrue(calendarSelector.setNumberOfChildrenForRoom(2, 1));
+    }
+
+    @Test(priority = 6)
+    public void setInfantsInTheRoom(){
+        calendarSelector.openTravelersPopUp();
+        Assert.assertTrue(calendarSelector.setNumberOfInfantsForRoom(3, 3));
+    }
+
+    @Test(priority = 7)
+    public void selectAge10ForTheChild(){
+        calendarSelector.openTravelersPopUp();
+        calendarSelector.setNumberOfChildrenForRoom(5, 1);
+        Assert.assertTrue(calendarSelector.selectValueFromChildrenDropDown(1, "10", 4));
+    }
+
+    @Test(priority = 7)
+    public void selectAgeUnder1ForTheInfant(){
+        calendarSelector.openTravelersPopUp();
+        calendarSelector.setNumberOfInfantsForRoom(3, 1);
+        Assert.assertTrue(calendarSelector.selectValueFromInfantsDropDown(1, "Under 1", 2));
+    }
+
+
 }
